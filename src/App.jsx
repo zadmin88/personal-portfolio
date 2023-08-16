8864bcbb7da8d015c460bb240c55ab40ec97bf89
Simon Franco Angarita <zaimon88@gmail.com>
2023-08-16 13:30:23 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="h-[800px]  font-Monserrat bg-zinc-900 flex flex-col justify-center w-screen px-6">
+      <h1 className="text-9xl text-white font-semibold tracking-normal">
+        SOFTWARE <br />
+        DEVELOPER
+      </h1>
+      <p className="text-lime-300 text-2xl tracking-widest">
+        FRONTEND - FULL STACK CAPABLE
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
